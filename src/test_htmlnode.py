86856fbf6a87991cd695180fe2e5548a16165b22
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-05-06 17:59:22 +0700

    Added htmlnode.py and test_htmlnode.py

---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode('h1', 'Header Text')
+        node2 = HTMLNode('h1', 'Header Text')
+        self.assertEqual(node, node2)
+    def test_defaults(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+    def test_props_to_html(self):
+        expected = ' href="https://www.google.com" target="_blank"'
+        node = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
+        actual = node.props_to_html()
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
