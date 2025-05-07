f7ee19c46767fd7c2582bace67d64e6293de083c
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-05-07 16:21:51 +0700

    Added extract_markdown_images and extract_markdown_links functions to tools.py

--- src/test_tools.py
+++ src/test_tools.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tools import text_node_to_html_node, split_nodes_delimiter
+from tools import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestTools(unittest.TestCase):
@@ -24,6 +24,16 @@ class TestTools(unittest.TestCase):
     def test_split_nodes_delimiter_2(self):
         node = TextNode("*We have a shortage of bold men", TextType.TEXT)
         self.assertRaises(ValueError, split_nodes_delimiter, [node], "*", TextType.BOLD)
+    def test_extract_markdown_images_0(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    def test_extract_markdown_links_0(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://google.com)"
+        )
+        self.assertListEqual([("link", "https://google.com")], matches)
 
 if __name__ == "__main__":
     unittest.main()
--- src/tools.py
+++ src/tools.py
@@ -1,3 +1,4 @@
+import re
 from htmlnode import LeafNode
 from textnode import TextNode, TextType
 
@@ -36,3 +37,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(split_text[i], TextType.TEXT))
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
